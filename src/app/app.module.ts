c9f39b2adb36e08c6a6f2474264462b3b935efee
FrancescoBadiali <francesco.badiali@globant.com>
2025-02-25 14:14:22 +0100

    fix versions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { InfoComponent } from './info/info.component';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { InfoComponent } from "./info/info.component";
 
 @NgModule({
-    declarations: [AppComponent, InfoComponent],
-    imports: [BrowserModule, BrowserAnimationsModule, IonicModule.forRoot({ hardwareBackButton: false }), AppRoutingModule],
-    providers: [
-        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-    ],
-    bootstrap: [AppComponent],
-    exports: [InfoComponent]
+  declarations: [AppComponent, InfoComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    IonicModule.forRoot({ hardwareBackButton: false }),
+    AppRoutingModule,
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+  exports: [InfoComponent],
 })
 export class AppModule {}
